1ebdda5f26272d19dac2e13079318af1724ca1d2
mll <2371695227@qq.com>
2020-12-30 14:03:47 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Switch,Route, HashRouter} from 'react-router-dom';
 import './App.css';
+import Home from './views/Home';
+import About from './views/About';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super();
+    this.state={};
+  }
+  render(){
+    return (
+      <HashRouter>
+        <Switch>
+          <Route component={Home}  exact path='/' />
+          <Route component={About} path='/about' />
+        </Switch>
+      </HashRouter>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/views/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+class About extends React.Component{
+    constructor(props){
+        super();
+        this.state={}
+    }
+    render(){
+        return <div>About</div>
+    }
+}
+export default About;
